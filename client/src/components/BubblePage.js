b314ad01a413d4c28ef47fa8378e628d24cb0221
Danielle B <danibaxx@hotmail.com>
2019-11-10 18:00:40 -0700

    Composed and Finished Login and able to login, rendered blank page due to component not being added to App.js, Composed ProtectedRoute to hide if user wasn't authenticated, now rendering page

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -8,10 +8,15 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-
+  axios
+    .get('http://localhost:5000/api')
+    
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList 
+        colors={colorList} 
+        updateColors={setColorList} 
+      />
       <Bubbles colors={colorList} />
     </>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [error, setError] = useState()
+  const [data, setData] = useState({
+    username: '',
+    password: '',
+  })
+
+  const handleChange = e => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    axios
+      .post('http://localhost:5000/api/login', data)
+      .then(result => {
+        localStorage.setItem('payload', result.data.payload)
+        props.history.push('/bubblepage')
+      })
+      .catch(err => {
+        setError(err.result.data)
+      })
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        {error && <div className='error'>{error}</div>}
+
+        <input 
+          type='text' 
+          name='username' 
+          placeholder='Username'
+          value={data.username}
+          onChange={handleChange} 
+        />
+        <input 
+          type='password' 
+          name='password' 
+          placeholder='Password'
+          value={data.password}
+          onChange={handleChange}  
+        />
+
+        <button type='submit'>Login</button>
+      </form>
     </>
   );
 };
